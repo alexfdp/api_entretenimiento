7bb23ea651a94547a1453e9850a76e3ad1dc1155
Alex Fajardo <alexf.124@hotmail.com>
2023-08-13 10:43:53 -0500

    cambios 6

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(pruebaRoutes)
 app.use('/api', usersRoutes)
 
-app.user((req, res, next) => {
+app.use((req, res, next) => {
     res.status(404).json({
         message: 'Endpoint not found'
     })
